1910292362e186089dc21f558ed0468d832d4b90
Atharva Joshi <Atharva.Joshi@dell.com>
2021-02-15 19:44:43 +0530

    delete scope
    Signed-off-by: Atharva Joshi <Atharva.Joshi@dell.com>

--- pravega-client-examples/src/main/java/io/pravega/example/debuggability/ScopeCreation.java
+++ pravega-client-examples/src/main/java/io/pravega/example/debuggability/ScopeCreation.java
@@ -17,13 +17,21 @@ public class ScopeCreation {
     
     private void createScope() {
         StreamManager streamManager = StreamManager.create(URI.create(uri));
-        streamManager.createScope(scopeName);
+        boolean success = streamManager.createScope(scopeName);
+        System.out.println(success);
+    }
+    
+    private void deleteScope() {
+        StreamManager streamManager = StreamManager.create(URI.create(uri));
+        boolean success = streamManager.deleteScope(scopeName);
+        System.out.println(success);
     }
     
     private static Options getOptions() {
         final Options options = new Options();
         options.addOption("u", "uri", true, "Controller URI");
         options.addOption("s", "scopeName", true, "scope name");
+        options.addOption("d", "scopeNameToDelete", false, "delete scope");
         return options;
     }
 
@@ -45,8 +53,12 @@ public class ScopeCreation {
         }
         String u = cmd.getOptionValue("uri");
         String s = cmd.getOptionValue("scopeName", "scope");
+        boolean toDelete = cmd.hasOption("d");
         ScopeCreation sc = new ScopeCreation(u, s);
-        sc.createScope();
+        if(toDelete)
+            sc.deleteScope();
+        else
+            sc.createScope();
     }
     
 }
